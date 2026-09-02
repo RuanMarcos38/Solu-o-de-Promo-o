74963aa9ca3f756f0315047a4b9580f22616959c
RuanMarcos38 <ruanessencia8@gmail.com>
2026-09-02 09:11:58 -0300

    Exibe painel de automacao IA no sistema

--- apps/web/src/entry.tsx
+++ apps/web/src/entry.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { AutomationDock } from './AutomationDock';
 import './styles.css';
 import './system-dashboard.css';
 
@@ -9,5 +10,6 @@ const rootElement = document.getElementById('root')!;
 createRoot(rootElement).render(
   <React.StrictMode>
     <App />
+    <AutomationDock />
   </React.StrictMode>
 );
