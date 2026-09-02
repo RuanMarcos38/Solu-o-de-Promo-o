4202b9a69a25a0cf72ec48eb1dfcf9f8bf3e97c4
RuanMarcos38 <ruanessencia8@gmail.com>
2026-09-02 09:12:27 -0300

    Importa estilos do painel de automacao

--- apps/web/src/entry.tsx
+++ apps/web/src/entry.tsx
@@ -4,6 +4,7 @@ import { App } from './App';
 import { AutomationDock } from './AutomationDock';
 import './styles.css';
 import './system-dashboard.css';
+import './automation-dock.css';
 
 const rootElement = document.getElementById('root')!;
 
